48afb7fb873a8950cdc947ea9e74cac198454b7e
wujing <bluehymn@qq.com>
2021-04-21 22:43:54 +0800

    update 20214222224354

--- deploy.cmd
+++ deploy.cmd
@@ -7,7 +7,7 @@ call npm run build
 git add .
 
 set DATE=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
-git commit -am "update ${DATE}" --allow-empty
+git commit -am "update %DATE%" --allow-empty
 git push
 
 cd .\public
